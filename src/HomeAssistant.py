bf57d1b3ee055f8b08ffcaacd7b59cb4deccee15
attennig <g.attenni96@gmail.com>
2021-06-25 16:27:39 +0200

    S2T + new structure of interaction

--- src/HomeAssistant.py
+++ src/HomeAssistant.py
@@ -1,97 +1,153 @@
 from src.Member import Member
 from src.Note import Note
+from src.SpeechEngine import SpeechEngine
 import pickle
 import os
-import pyttsx3
+import sys
 
 class HomeAssistant:
-    def __init__(self):
+    def __init__(self, blind=False, deaf=False, dumb=False):
         # load data from db
         self.members = []
         self.notes = []
         self.loadMembers()
-        self.voice = pyttsx3.init()
+        self.speechEngine = SpeechEngine()
+
+        self.AUDIO_IN = not dumb
+        self.AUDIO_OUT = not deaf
+        self.VIDEO_IN = not blind and (deaf or dumb)
+        self.VIDEO_OUT = not blind
+        if not ((self.AUDIO_IN or self.VIDEO_IN) and (self.AUDIO_OUT or self.VIDEO_OUT)):
+            self.speechEngine.say("Sorry, I cannot support this modality")
+            sys.exit(-5)
+
+
 
     def start(self):
         # start camera:Gabriel
-
         while True:
             if self.detectPresence():
                 member = self.recognizeMember()
                 if member == None:
-                    self.registerMember()
+                    self.nonMemberInteraction()
                 else:
-                    self.say(f"Hello, {member.name}")
-                    notes = self.getNotesTo(member)
-                    if len(notes) == 0:
-                        self.say(f"There are no notes left for you")
+                    self.memberInteraction(member)
+
+
+    '''Interactions'''
+    def nonMemberInteraction(self):
+        self.speechEngine.say("Seems your not part of this Clan, do you want to register?")
+        answer = self.speechEngine.listen()
+        if "yes" in answer.split():
+            self.registerMember()
+
+    def memberInteraction(self, member):
+        while True:
+            self.speechEngine.say(f"Hello, {member.name[0].capitalize()+member.name[1:]}")
+            if not self.isMemberStillHere(member): break
+            notes = self.getNotesTo(member)
+            if len(notes) == 0:
+                self.speechEngine.say(f"There are no notes left for you")
+            else:
+                self.speechEngine.say(f"I have found {len(notes)} notes for you, do you want to hear them? ")
+                answer = self.speechEngine.listen()
+                if "yes" in answer.split(): self.tellNotes(notes)
+            self.speechEngine.say(f"What can I do for you, {member.name[0].capitalize()+member.name[1:]}?")
+            answer = self.speechEngine.listen()
+            action = self.getMostProbableAction(answer)
+            self.speechEngine.say(f"So, do you want to {action} a note?")
+            answer = self.speechEngine.listen()
+            if "yes" in answer:
+                if action == "leave":
+                    self.leaveNoteInteraction(member)
+                else:
+                    self.noteInteraction(action, member)
+
+    def leaveNoteInteraction(self, member):
+        self.speechEngine.say(f"Who is the recipient of your note? ")
+        to_who = input(f"Write here ")
+        recipient = self.searchMemberNamed(to_who)
+        if recipient == None:
+            self.speechEngine.say(f"Sorry I couldn't find any member named {to_who}")
+        else:
+            self.speechEngine.say(f"What do you want to say to {recipient.name}?")
+            content = input(f"Write here ")
+            self.addNote(member, recipient, content)
+
+    def noteInteraction(self, type, member):
+        assert type == "edit" or type == "remove"
+        self.speechEngine.say(f"Which member is the recipient? ")
+        to_who = input("Write here ")
+        # check to_who is a member
+        recipient = self.searchMemberNamed(to_who)
+        if recipient == None:
+            self.speechEngine.say(f"Sorry I couldn't find any member with such name")
+        else:
+            notes_to_who = self.getNotesFromTo(member, recipient)
+            self.speechEngine.say(f"I have found {len(notes_to_who)} from you to {recipient.name}")
+            if len(notes_to_who) == 0:
+                print("ERROR: there are no notes!! ")
+                return
+            if len(notes_to_who) == 1:
+                # there is just one note
+                if type == "edit":
+                    self.editNote(notes_to_who[0])
+                else:
+                    self.removeNote(notes_to_who[0])
+            else:
+                self.speechEngine.say(f"You need to be more specific. Try typing some words in the note\n")
+                words = input("Write here ")
+                most_prob_note = self.getMostProbableNote(notes_to_who, words.split())
+                self.speechEngine.say(f"Do you want to edit this note? \"{most_prob_note.content}\"")
+                answer = input("Write here ")
+                if answer == "yes":
+                    if type == "edit":
+                        self.editNote(most_prob_note)
                     else:
-                        self.say(f"I have found {len(notes)} notes for you, do you want to hear them? ")
-                        answer = input(f"Write here")
-                        if answer == "yes":
-                            for note in notes:
-                                self.say(f"Note from {note.sender.name}:\n{note.content}")
-                                self.removeNote(note)
-                    self.say(f"By the way do you want to leave any note? ")
-                    answer = input(f"Write here ")
-                    if answer == "yes":
-                        self.say(f"Who is the recipient of your note? ")
-                        to_who = input(f"Write here ")
-                        recipient = self.searchMemberNamed(to_who)
-                        if recipient == None:
-                            self.say(f"Sorry I couldn't find any member named {to_who}")
-                        else:
-                            self.say(f"What do you want to say to {recipient.name}?")
-                            content = input(f"Write here ")
-                            self.addNote(member, recipient, content)
-
-                    if len(self.getNotesFrom(member)) > 0:
-                        self.say(f"Do you want to edit any note?")
-                        answer = input(f"Write here ")
-                        if answer == "yes":
-                            self.noteInteraction("edit", member)
-                        self.say(f"Do you want to remove any note?")
-                        answer = input(f"Write here ")
-                        if answer == "yes":
-                            self.noteInteraction("remove", member)
-
+                        self.removeNote(most_prob_note)
+    def tellNotes(self, notes):
+        for note in notes:
+            self.speechEngine.say(f"Note from {note.sender.name}:\n{note.content}")
+            self.removeNote(note)
 
     def detectPresence(self):
         # Gabriel
         return True
 
+    def isMemberStillHere(self, member):
+        return self.detectPresence() #and (self.recognizeMember() == member)
+
     '''Members'''
+
     def recognizeMember(self):
         # Gabriel
-        self.say(f"Authentication\nWhat is your name? ")
-        name = input("Write here ")
+        self.speechEngine.say(f"Authentication\nWhat is your name? ")
+        name = self.speechEngine.listen()
         return self.searchMemberNamed(name)
 
     def registerMember(self):
-        self.say(f"Registration\nWhat is your name? ")
-        name = input("Write here ")
+        self.speechEngine.say(f"Registration\nWhat is your name? ")
+        name = self.speechEngine.listen()
         # Gabriel: take pictures
         pictures = None
-        newMember = Member(name, pictures)
+        newMember = Member(name.lower(), pictures)
         self.members.append(newMember)
         self.storeMembers()
+        self.speechEngine.say("Registration completed")
 
     def storeMembers(self):
         members_dict = {}
         for member in self.members:
             members_dict[member.name] = member.pictures
         pickle.dump(members_dict, open("../data/members.p", "wb"))
-        #pickle.dump(self.members, open("../data/members.p", "wb"))
 
     def loadMembers(self):
         self.members = []
         if os.path.getsize("../data/members.p") > 0:
-            #self.members = pickle.load(open("../data/members.p", "rb"))
             members_dict = pickle.load(open("../data/members.p", "rb"))
             for name in members_dict:
                 self.members += [Member(name, members_dict[name])]
         self.loadNotes()
-
 
     def searchMemberNamed(self, name):
         for member in self.members:
@@ -100,6 +156,7 @@ class HomeAssistant:
         return None
 
     '''Notes'''
+
     def storeNotes(self):
         # esempio: {"Antonio": [NoteObj, NoteObj],, "Fabio": [NoteObj, NoteObj]}
         notes_dict = {}
@@ -110,12 +167,10 @@ class HomeAssistant:
                 notes_dict[note.recipient.name] = [{'sender': note.sender.name, 'content': note.content}]
 
         pickle.dump(notes_dict, open("../data/notes.p", "wb"))
-        #pickle.dump(self.notes, open("../data/notes.p", "wb"))
 
     def loadNotes(self):
         self.notes = []
         if os.path.getsize("../data/notes.p") > 0:
-            #self.notes = pickle.load(open("../data/notes.p", "rb"))
             notes_dict = pickle.load(open("../data/notes.p", "rb"))
             for member in self.members:
                 if member.name in notes_dict.keys():
@@ -133,15 +188,15 @@ class HomeAssistant:
         self.storeNotes()
 
     def editNote(self, edit_note):
-        self.say("Do you want to change the content? ")
+        self.speechEngine.say("Do you want to change the content? ")
         answer = input("Write here ")
         if answer == "yes":
-            self.say("Write the new content ")
+            self.speechEngine.say("Write the new content ")
             edit_note.content = input("Write here ")
-        self.say("Do you want to change the recipient ")
+        self.speechEngine.say("Do you want to change the recipient ")
         answer = input("Write here ")
         if answer == "yes":
-            self.say("Tell me the name of the new recipient ")
+            self.speechEngine.say("Tell me the name of the new recipient ")
             to_who = input("Write here ")
             recipient = self.searchMemberNamed(to_who)
             edit_note.recipient = recipient
@@ -177,40 +232,9 @@ class HomeAssistant:
                 max = len([word for word in words if word in note.content])
         return max_note
 
-    def noteInteraction(self, type, member):
-        assert type == "edit" or type == "remove"
-        self.say(f"Which member is the recipient? ")
-        to_who = input("Write here ")
-        # check to_who is a member
-        recipient = self.searchMemberNamed(to_who)
-        if recipient == None:
-            self.say(f"Sorry I couldn't find any member with such name")
-        else:
-            notes_to_who = self.getNotesFromTo(member, recipient)
-            self.say(f"I have found {len(notes_to_who)} from you to {recipient.name}")
-            if len(notes_to_who) == 0:
-                print("ERROR: there are no notes!! ")
-                return
-            if len(notes_to_who) == 1:
-                # there is just one note
-                if type == "edit":
-                    self.editNote(notes_to_who[0])
-                else:
-                    self.removeNote(notes_to_who[0])
-            else:
-                self.say(f"You need to be more specific. Try typing some words in the note\n")
-                words = input("Write here ")
-                most_prob_note = self.getMostProbableNote(notes_to_who, words.split())
-                self.say(f"Do you want to edit this note? \"{most_prob_note.content}\"")
-                answer = input("Write here ")
-                if answer == "yes":
-                    if type == "edit":
-                        self.editNote(most_prob_note)
-                    else:
-                        self.removeNote(most_prob_note)
-
-
-    def say(self, text):
-        print(text)
-        self.voice.say(text)
-        self.voice.runAndWait()
+    def getMostProbableAction(self, text):
+        words = text.split()
+        prob = {"edit": len([w for w in words if w in ["edit", "modity", "change"]])/len(words),
+                "remove": len([w for w in words if w in ["delete", "remove", "forget"]])/len(words),
+                "leave": len([w for w in words if w in ["new", "leave", "say", "tell"]])/len(words)}
+        return max(prob, key=lambda key: prob[key])
